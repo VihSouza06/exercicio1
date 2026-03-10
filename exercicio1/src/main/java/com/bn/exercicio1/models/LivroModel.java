e74b3b09bfea3a7c6d21c321e7dd4d3cf8f655d9
VihSouza06 <visouzasantos2006@gmail.com>
2026-03-10 20:31:25 -0300

    Modificacoes no Model

--- exercicio1/src/main/java/com/bn/exercicio1/models/LivroModel.java
+++ exercicio1/src/main/java/com/bn/exercicio1/models/LivroModel.java
@@ -1,20 +1,22 @@
 package com.bn.exercicio1.models;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
-@Entity
+@Entity (name = "Livro")
+@Table (name = "TBL_LIVRO")
 public class LivroModel {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
 
     private Long id;
-    private String titulo;
-    private String autor;
     private Integer anoPublicacao;
+
+    @Column(name = "nome_livro")
+    private String titulo;
+
+    @Column(name = "nome_autor")
+    private String autor;
 
     public LivroModel(){
 
